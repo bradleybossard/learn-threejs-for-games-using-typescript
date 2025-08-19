e9f1439a96abdb59878437a70129757d27809083
Bradley Bossard <bradleybossard@gmail.com>
2025-08-18 21:08:53 -0700

    render basic black screen

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
 import * as THREE from 'three';
+
+const width = window.innerWidth
+const height = window.innerHeight
+
+const renderer = new THREE.WebGLRenderer({
+	canvas: document.getElementById('app') as HTMLCanvasElement
+})
+renderer.setSize(width, height)
+
+const mainCamera = new THREE.PerspectiveCamera(60, width / height, 0.1, 100)
+
+const scene = new THREE.Scene()
+
+renderer.render(scene, mainCamera)
