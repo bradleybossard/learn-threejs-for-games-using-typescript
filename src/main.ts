eb8d67c1d60c07079a7f5865991eb0621e37a5c8
Bradley Bossard <bradleybossard@gmail.com>
2025-08-18 21:15:45 -0700

    render basic cube

--- src/main.ts
+++ src/main.ts
@@ -12,4 +12,17 @@ const mainCamera = new THREE.PerspectiveCamera(60, width / height, 0.1, 100)
 
 const scene = new THREE.Scene()
 
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshPhongMaterial({ color: 0x00ff00 })
+
+const cube = new THREE.Mesh(geometry, material)
+cube.position.set(0, 1, -5)
+scene.add(cube)
+
+const light = new THREE.DirectionalLight(0xffffff, 1)
+light.position.set(1, 1, 1)
+scene.add(light)
+
+
+
 renderer.render(scene, mainCamera)
